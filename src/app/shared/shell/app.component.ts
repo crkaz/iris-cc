d399373a1a78ff48ab394643fc8d0cbacf6d0357
Zak (Desktop) <zakcatherall@outlook.com>
2020-01-23 15:35:22 +0000

    Modified logo and app.component.ts to easily configure header

--- src/app/shared/shell/app.component.ts
+++ src/app/shared/shell/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 
+
+// Valid options for toolbar colour.
+const enum ToolbarColour { blue = "primary", pink = "accent", red = "warn", grey = "" };
+const ASSET_PATH: string = "../../../assets/";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'IRIS CC';
-  slogan = "Command centre for IRIS enabled patients.";
-  toolbarColour = ""; // Primary, warn, accent, "" (grey).
+  private readonly logo: string = "logo-2-lg-wcg-bi-cr";
+  
+  public readonly toolbarColour: string = ToolbarColour.blue; // primary, warn, accent, "" (grey).
+  public readonly title: string = 'IRIS CC';
+  public readonly slogan: string = "Command centre for IRIS enabled patients.";
+  public readonly logoPath = ASSET_PATH + this.logo + ".png";
 }
